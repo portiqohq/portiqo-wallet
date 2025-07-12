159987182bee446322b54231b8d32fe366807e46
Nelson Healy <nelson@apus.ventures>
2025-07-11 21:42:10 -0400

    Added empty views to view card details and view/edit card details

---
+++ Portiqo Wallet/Views/CardDetailsView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct CardDetailsView: View {
+    var body: some View {
+        Text("This is where you could learn about a Keycard")
+        Text("But there's nothing here yet :(")    }
+}
+
+#Preview {
+    CardDetailsView()
+}
---
+++ Portiqo Wallet/Views/CloneCardView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct CloneCardView: View {
+    var body: some View {
+        VStack {
+            Text("This is where you would clone a Keycard")
+            Text("But there's nothing here yet :(")
+        }
+    }
+}
+
+#Preview {
+    CloneCardView()
+}
