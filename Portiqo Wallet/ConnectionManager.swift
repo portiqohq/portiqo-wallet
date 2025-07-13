a0b8b5f78dda0efaf4da9d1ff61412df1b90a699
Nelson Healy <nelson@apus.ventures>
2025-07-13 11:48:07 -0400

    Added simulated hardware delays and dummy returns to connectionmanager functions

--- Portiqo Wallet/ConnectionManager.swift
+++ Portiqo Wallet/ConnectionManager.swift
@@ -13,11 +13,13 @@ class ConnectionManager {
 
     /// Establishes a connection to the Portiqo Key
     func connect() async {
+        try? await Task.sleep(nanoseconds: 500_000_000) // 0.5s to simulate HW delay
         self.isKeyConnected = true
     }
 
     /// Terminates connection to the Portiqo Key
     func disconnect() async {
+        try? await Task.sleep(nanoseconds: 200_000_000) // 0.2s to simulate HW delay
         self.isKeyConnected = false
     }
 
@@ -25,18 +27,25 @@ class ConnectionManager {
     /// - Parameters:
     ///     - card: The keycard to be written
     func sendCard(_ card: Keycard) async {
+        try? await Task.sleep(nanoseconds: 5_000_000_000) // 0.5s to simulate HW delay/
         self.currentCard = card.id
     }
 
     /// Reads a physical RF Keycard and creates a Keycard object with its data
     /// - Returns: A Keycard object with the information read from the physical card
-    func cloneCard() async -> Keycard? { return nil }
+    func cloneCard() async -> Keycard? {
+        try? await Task.sleep(nanoseconds: 2_000_000_000) // 2s delay to simulate user tapping card to Portiqo Key
+        return Keycard(name: "Cloned Card")
+    }
 
     /// Fetches the ID of the Keycard currently stored on the Portiqo Key
     /// - Returns:
     ///     - If a Keycard is currently stored on the Portiqo Key, the ID of that Keycard
     ///     - If no keycard is currently stored on the Portiqo Key, nil.
-    func getCurrentCard() async -> UUID? { return nil }
+    func getCurrentCard() async -> UUID? {
+        try? await Task.sleep(nanoseconds: 1_000_000_000) // 0.1s to simulate HW delay
+        return UUID()
+    }
 
 }
 
